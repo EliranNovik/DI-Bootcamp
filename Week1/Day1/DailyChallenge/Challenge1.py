9c9edd75f24f4c17ff557ea9ac01bcb7c36b1483
EliranNovik <eliran.novik@gmail.com>
2024-11-11 18:50:02 +0200

    finished

---
+++ Week1/Day1/DailyChallenge/Challenge1.py
@@ -0,0 +1,50 @@
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+
+print(99**3 * 8)
+
+# false
+# True
+# False
+# False
+# Error
+# False
+
+computer_brand='Mac'
+print(f"I have a {computer_brand} computer")
+
+name='Eliran'
+age=29
+shoe_size=45
+
+info= f"My name is {name}, I am {age} years old, and my shoe size is {shoe_size}."
+
+
+    a=10
+    b=5
+
+    if a > b:
+        print("Hello World")
+
+number = int(input("enter a number: "))
+
+if number % 2 == 0:
+    print("number is even.")
+else:
+    print("number is odd.")
+
+my_name = "Eliran" 
+user_name = input("What's your name? ")
+if user_name == my_name:
+    print("Are we related?")
+else:
+    print(f"Hi, {user_name} good luck with that name!")
+
+ height = int(input("Please enter your height in centimeters: "))
+  if height > 145:
+    print("You are tall enough to ride!")
+else:
+    print("Sorry, you need to grow some more to ride.")
+
